4ad5d4f3c75c19c0d456a34fd63a2b7413c330c7
Shripraksh Tiwari <57065429+shriprakashtiwari@users.noreply.github.com>
2025-04-13 08:41:35 +0530

    backend / theme

--- apps.py
+++ apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class CustomerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customer'
+class ThemeConfig(AppConfig):
+    name = 'theme'
